3f75dde1f7747a697b22af183c0e50f987e5ce24
hankyul <consistant1y@ajou.ac.kr>
2021-09-08 16:41:36 +0900

    refactor: Refactor train
    - Add comments
    - Split Dataset and model def
    - make run function

--- src/train.py
+++ src/train.py
@@ -4,13 +4,12 @@ from torch_geometric.nn import GCNConv
 
 from torch_geometric.datasets import Planetoid
 
-dataset = Planetoid(root='Cora', name='Cora')
 
 class Net(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, in_features, nclass):
         super().__init__()
-        self.conv1 = GCNConv(dataset.num_node_features, 16)
-        self.conv2 = GCNConv(16, dataset.num_classes)
+        self.conv1 = GCNConv(in_features, 16)
+        self.conv2 = GCNConv(16, nclass)
     
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
@@ -20,27 +19,36 @@ class Net(torch.nn.Module):
         x = self.conv2(x, edge_index)
         
         return F.log_softmax(x, dim=1)
-    
-device = torch.device('cpu')
-model = Net().to(device)
-data = dataset[0].to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
 
+def run(args):
+    device = torch.device('cpu')
 
-print_freq = 20
-model.train()
-for epoch in range(200):
-    optimizer.zero_grad()
-    out = model(data)
-    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
-    loss.backward()
-    optimizer.step()
-    
-    if epoch!=0 and epoch % print_freq == 0:
-        print('epoch: {}/{} loss: {:6.4f}'.format(epoch+1, 200, loss.detach().item()))
-    
-model.eval()
-_, pred = model(data).max(dim=1)
-correct = int(pred[data.test_mask].eq(data.y[data.test_mask]).sum().item())
-acc = correct / int(data.test_mask.sum())
-print('Accuracy: {:.4f}'.format(acc))
+    # step 1. load dataset
+    dataset = Planetoid(root='Data', name='Cora')
+    data = dataset[0].to(device)
+
+    # step 2. load model
+    model = Net(dataset.num_node_features, dataset.num_classes).to(device)
+
+    # step 3. prepare training tool
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
+
+    # step 4. train
+    print_freq = 20
+    model.train()
+    for epoch in range(200):
+        optimizer.zero_grad()
+        out = model(data)
+        loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+        loss.backward()
+        optimizer.step()
+
+        if epoch!=0 and epoch % print_freq == 0:
+            print('epoch: {}/{} loss: {:6.4f}'.format(epoch+1, 200, loss.detach().item()))
+
+    # step 5. evaluate
+    model.eval()
+    _, pred = model(data).max(dim=1)
+    correct = int(pred[data.test_mask].eq(data.y[data.test_mask]).sum().item())
+    acc = correct / int(data.test_mask.sum())
+    print('Accuracy: {:.4f}'.format(acc))
